c231764f100aa3be13faa1ab4181c43000208728
Loïc FAVRELIERE <17621704+lfavreli@users.noreply.github.com>
2024-08-27 16:49:51 +0200

    feat: improves StaticRouter operation

--- src/main/java/fr/lfavreli/clc/config/StaticRouterConfig.java
+++ src/main/java/fr/lfavreli/clc/config/StaticRouterConfig.java
@@ -1,31 +1,50 @@
 package fr.lfavreli.clc.config;
 
-import java.util.Arrays;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
 
 @Configuration(proxyBeanMethods = false)
 public class StaticRouterConfig {
 
     private static final String STATIC_INDEX_HTML = "static/index.html";
 
+    /**
+     * Returns a router function that serves static resources from the 'static' directory.
+     *
+     * If a request is made to the root URL ('/'), it returns the index.html file. All other
+     * requests are permanently redirected to the root URL.
+     *
+     * @return a router function for serving static resources and handling redirects
+     */
     @Bean
     public RouterFunction<ServerResponse> staticRouter() {
-        ClassPathResource index = new ClassPathResource(STATIC_INDEX_HTML);
-        List<String> extensions = Arrays.asList("js", "css", "svg", "png", "jpg", "woff2", "json", "webmanifest", "pdf");
-        RequestPredicate staticPredicate = RequestPredicates.path("/api/**")
-                .or(RequestPredicates.pathExtension(extensions::contains))
-                .negate();
+        return RouterFunctions.resources("/**", new ClassPathResource("static/"))
+                .andRoute(RequestPredicates.GET("/"), this::indexHandler)
+                .andRoute(RequestPredicates.GET("/**"), this::redirectToIndex);
+    }
 
-        return RouterFunctions.route().resource(staticPredicate, index).build();
+    /**
+     * @param serverRequest (unused)
+     */
+    private Mono<ServerResponse> indexHandler(ServerRequest serverRequest) {
+        return ServerResponse.ok().bodyValue(new ClassPathResource(STATIC_INDEX_HTML));
+    }
+
+    /**
+     * @param serverRequest (unused)
+     */
+    private Mono<ServerResponse> redirectToIndex(ServerRequest serverRequest) {
+        return ServerResponse.permanentRedirect(URI.create("/")).build();
     }
 
 }
